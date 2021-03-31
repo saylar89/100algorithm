dd5450c5f462bbe458fdb87bc0397e1f834ae938
Aslan Ashtiani <saylar.design@gmail.com>
2021-03-31 16:22:31 +0430

    solve 4

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
 export function addTwoDigits(n: any): number {
+  const nums = n.toString().split("");
 
+  return parseInt(nums[0]) + parseInt(nums[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
