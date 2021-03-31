850e5fb09dd8d69b204c16a78b7e8a474bc9dbff
Aslan Ashtiani <saylar.design@gmail.com>
2021-03-31 17:17:41 +0430

    test

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,17 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+     let longestLength = 0;
+     const longestWords = [];
 
+     inputArray.forEach((word:string) => {
+         longestLength = longestLength<word.length? word.length:longestLength;
+     })
+
+     inputArray.forEach((word:string)=> {
+         if(longestLength === word)
+     })
+
+   return longestWords;
+     
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
